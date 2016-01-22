306aa46a18ec4f6c78372d02d490b94d970830e2
Scott Dial <scott@scottdial.com>
2016-01-22 09:19:19 -0500

    Remove unnecessary steps from the init sequence.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Remove all of the built-in services
+rm -rf /etc/service/*
+
+# Remove all of the built-in init scripts
+rm -rf /etc/my_init.d/*
+
+# Create a service for each pen_cmd.* or .*_pen_cmd.* environment variable
 env | grep -e '^pen_cmd.*' -e '.*_pen_cmd.*' | while read line; do
   name=$(echo "$line" | cut -d= -f1)
   args=$(echo "$line" | cut -d= -f2- | envsubst)
@@ -12,4 +19,5 @@ EOF
   chmod +x "/etc/service/$name/run"
 done
 
+# Switch over to the init process
 exec /sbin/my_init
